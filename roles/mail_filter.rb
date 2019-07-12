8e13399d07530a2b41c9672149e5d431193fbf1d
Nevyn McAnferny <nevyn@anferny.me.uk>
2019-07-12 15:09:39 +0100

    Forgot to stage role file

--- roles/mail_filter.rb
+++ roles/mail_filter.rb
@@ -1,11 +1,12 @@
 name "mail_filter"
 description "mail filter role"
-run_list ["recipe[postfix::server]"]
+run_list ["recipe[postfix]"]
 override_attributes(
   "postfix" => {
     "mail_type" => "master",
     "main" => {
       "mynetworks" => [ "10.3.3.0/24", "127.0.0.0/8" ],
+	  mydestination => "$myhostname, localhost.$mydomain, localhost, mail.$mydomain, mail, $mydomain, cakecrumbs.co.uk, cake-crumbs.co.uk, 45throssendale.org.uk, tjsanalytic.co.uk",
       "inet_interfaces" => "all",
       "mydomain" => "anferny.me.uk",
       "myorigin" => "anferny.me.uk"
